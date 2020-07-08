8b6154619cf99b45eb5b4a4bdc9db89e2080aafb
Emil Teddy Stachurska <36759138+emilteddy@users.noreply.github.com>
2020-07-08 09:05:24 +0200

    Add files via upload

---
+++ Masterscript.R
@@ -0,0 +1,31 @@
+##### 1.0 Data processing ----------------------------------------------------------------------------------------------------------
+### 1.1 Preliminaries
+source("00. Globale indstillinger/Preliminaries.R", encoding = "UTF-8-BOM")
+
+### 1.2 Recoding
+source("06. Data/02. Rekodning/02. script/Recoding.R", encoding = "UTF-8-BOM")
+
+### 1.3 Cleaning
+source("06. Data/03. Datarens/02. script/Cleaning.R", encoding = "UTF-8-BOM")
+
+### 1.4 Monitor
+source("06. Data/04. Monitor/02. script/Monitoring.R", encoding = "UTF-8-BOM")
+
+### 1.5 Merge
+source("06. Data/05. Merge/02. script/Merging.R", encoding = "UTF-8-BOM")
+
+##### 2.0 Analysis -----------------------------------------------------------------------------------------------------------------
+### 2.1 Overview of results
+source("07. Analyse/01. Conjointeksperiment/02. script/Overview.R", encoding = "UTF-8-BOM")
+
+### 2.2 Hierarchy hypothesis
+source("07. Analyse/02. Kulturhypoteserne/02. script/Hierarchy.R", encoding = "UTF-8-BOM")
+
+### 2.3 Consensus hypothesis
+source("07. Analyse/03. Økonomihypoteserne/02. script/Consensus.R", encoding = "UTF-8-BOM")
+
+### 2.4 Priming hypothesis
+source("07. Analyse/04. Priminghypotesen/02. script/Priming.R", encoding = "UTF-8-BOM")
+
+### 2.5 Robust checks
+source("07. Analyse/05. Robusthedstest/02. script/Robust.R", encoding = "UTF-8-BOM")
